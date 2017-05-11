ad1f43187ea9a8c59777e8d5b9496ef77d5abfc2
khj <adsf465@naver.com>
2017-05-11 09:55:31 +0900

    ddd

--- TeamPJTEST/src/GUIng/MainFrame.java
+++
@@ -1,9 +0,0 @@
-package GUIng;
-
-
-public class MainFrame  {
-	
-	
-}
-
-
--- TeamPJTEST/src/GUIng/OOP_API.java
+++
@@ -1,9 +0,0 @@
-package GUIng;
-
-public class OOP_API {
-
-	public static void main(String[] args) {
-		GUI ma=new GUI();
-	}
-
-}
